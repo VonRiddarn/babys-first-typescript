d8cb05c7d47fc90225282430e2232026a962a458
VonRiddarn <vonriddarn@gmail.com>
2024-12-02 13:40:34 +0100

    feat: first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+var x = 12;
+var y = 12;
+var z = 12; // Bad practice
+y = "12";
+console.log(x);
+console.log(y);
---
+++ test.ts
@@ -0,0 +1,8 @@
+let x: number = 12;
+let y: number | string = 12;
+let z: any = 12; // Bad practice
+
+y = "12";
+
+console.log(x);
+console.log(y);
